6e63e79887cb715b0a4e78a62c983d0e28ee7a1a
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-09-22 19:34:35 -0300

    feat: initial commmit

---
+++ src/main/java/com/themyntt/dao_cae_demo/DaoCaeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.themyntt.dao_cae_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoCaeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaoCaeDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/themyntt/dao_cae_demo/DaoCaeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.themyntt.dao_cae_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoCaeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
