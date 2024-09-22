67a8b08f0089458398e5ab7991b954e79085fb7b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-09-22 20:14:48 -0300

    feat: create todo controller

---
+++ src/main/java/com/themyntt/dao_cae_demo/adapters/io/inputs/TodoCreationController.java
@@ -0,0 +1,21 @@
+package com.themyntt.dao_cae_demo.adapters.io.inputs;
+
+import com.themyntt.dao_cae_demo.assemblers.use_cases.todo_creation.TodoCreationUseCaseAssembler;
+import com.themyntt.dao_cae_demo.core.use_cases.todo_creation.TodoCreationUseCase;
+import com.themyntt.dao_cae_demo.core.use_cases.todo_creation.io.inputs.TodoCreationUseCaseInput;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/api/todo/create")
+public class TodoCreationController {
+    private static final TodoCreationUseCase useCase = TodoCreationUseCaseAssembler.V1;
+
+    @PostMapping
+    public ResponseEntity<?> perform(@RequestBody TodoCreationUseCaseInput input) {
+        return useCase.run(input);
+    }
+}
